2709dc205dd7d2a92ea6b5f9042d992581587608
Eka Putra Prasetya <eka.putra@caditglobal.com>
2026-02-18 05:18:40 +0700

    card

--- app/_landing-page/card.tsx
+++ app/_landing-page/card.tsx
@@ -7,11 +7,11 @@ export default function Card({ title, disabled = false}: CardProps) {
   const dispatch = useDispatch();
 
     return (
-    <div onClick={() => dispatch(changeSwitcherVal({page: "detail", selectedPage: "detail"}))} className={`backdrop-blur-md bg-white/30 rounded-2xl p-6 flex flex-col items-center justify-center hover:scale-105 transition ${disabled ? "opacity-50 grayscale cursor-not-allowed pointer-events-none" : "cursor-pointer"}`}>
-      <div className="w-14 h-14 bg-green-200 rounded-full flex items-center justify-center mb-3">
-        🌱
+    <div onClick={() => dispatch(changeSwitcherVal({page: "detail", selectedPage: "detail"}))} className={`backdrop-blur-md bg-[#88ba66] rounded-2xl p-6 gap-2.5 flex flex-col items-center justify-center hover:scale-105 hover:bg-[#a7d87c] transition ${disabled ? "opacity-50  bg-[#A7D87C] cursor-not-allowed pointer-events-none" : "cursor-pointer"}`}>
+      <div className="w-32 h-32 bg-[#D3FFA9] rounded-full flex items-center justify-center mb-3">
+        
       </div>
-      <h2 className="text-xl font-semibold text-black">{title}</h2>
+      <h2 className="text-[48px] text-center font-semibold text-black">{title}</h2>
     </div>
     )   
 }
