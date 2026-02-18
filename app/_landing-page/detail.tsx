45ca62903357f0d9a23870b7977505858b5a07d0
Eka Putra Prasetya <eka.putra@caditglobal.com>
2026-02-18 15:15:50 +0700

    add breadcrumb, dynamic layout, and information container

--- app/_landing-page/detail.tsx
+++ app/_landing-page/detail.tsx
@@ -1,25 +1,23 @@
-import { useDispatch } from "react-redux"
-import { changeSwitcherVal } from "@/Redux/store/slices/landingPageSwitcherSlice";
-
+import { useSelector } from "react-redux"
+import DetailDynamicLayout from "./detail-dynamic-layout";
+import InformationContainer from "./information-container"
+import Breadcrumb from "../_components/breadcrumb/breadcrumb";
+import { RootState } from "@/Redux/store";
 
 export default function DetailLandingPage(){
-    const dispatch = useDispatch();
+
+    const currentPage = useSelector((state: RootState) => state.landingPageSwitcher.selectedPage);
+
     return (
-        <>
-            <div className="bg-[linear-gradient(90deg,rgba(126,211,33,0.45)_0%,rgba(76,175,80,0.85)_100%)] w-full max-h-fit py-6 flex justify-center items-center flex-col gap-2 ">
-                <h1 className="text-7xl font-bold text-black">
-                    IoT Hydroponic Lab
-                </h1>
-                <h2 className="text-3xl text-black font-medium">
-                    Detailed View
-                </h2>
+        <div className="w-full h-full flex flex-col">
+            <div className="flex justify-between items-center">
+                <Breadcrumb currentPage={currentPage} />
+                <InformationContainer />
             </div>
-            <div className="py-4 w-full flex justify-center">
-                <button onClick={() => dispatch(changeSwitcherVal({page: "main", selectedPage: ""}))} className="px-6 py-3 rounded-lg bg-green-600 text-white text-lg font-medium hover:bg-green-700 transition-colors">
-                    Back to Main
-                </button>
+            <div className="w-full h-4/5">
+                <DetailDynamicLayout />
             </div>
-        </>
+        </div>
     )
 
 }
