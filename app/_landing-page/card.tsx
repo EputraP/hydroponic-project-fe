4adf9959ae7d4e32804a5eeb1645b4f58435383b
Eka Putra Prasetya <eka.putra@caditglobal.com>
2026-02-18 15:13:37 +0700

    add card detail option

--- app/_landing-page/card.tsx
+++ app/_landing-page/card.tsx
@@ -2,16 +2,25 @@ import { CardProps } from "../types"
 import { useDispatch } from "react-redux"
 import { changeSwitcherVal } from "@/Redux/store/slices/landingPageSwitcherSlice";
 
-export default function Card({ title, disabled = false}: CardProps) {
+export default function Card({ type, title, disabled = false}: CardProps) {
 
   const dispatch = useDispatch();
 
+  if(type === "main"){
     return (
-    <div onClick={() => dispatch(changeSwitcherVal({page: "detail", selectedPage: "detail"}))} className={`backdrop-blur-md bg-[#88ba66] rounded-2xl p-6 gap-2.5 flex flex-col items-center justify-center hover:scale-105 hover:bg-[#a7d87c] transition ${disabled ? "opacity-50  bg-[#A7D87C] cursor-not-allowed pointer-events-none" : "cursor-pointer"}`}>
+    <div onClick={() => dispatch(changeSwitcherVal({page: "detail", selectedPage: title}))} className={`backdrop-blur-md bg-[#88ba66] rounded-2xl p-6 gap-2.5 flex flex-col items-center justify-center hover:scale-105 hover:bg-[#a7d87c] transition ${disabled ? "opacity-50  bg-[#A7D87C] cursor-not-allowed pointer-events-none" : "cursor-pointer"}`}>
       <div className="w-32 h-32 bg-[#D3FFA9] rounded-full flex items-center justify-center mb-3">
         
       </div>
       <h2 className="text-[48px] text-center font-semibold text-black">{title}</h2>
     </div>
     )   
+  }else{
+    return (
+      <div className={`backdrop-blur-md bg-[#88ba66] rounded-2xl p-6 gap-2.5 flex flex-col items-center justify-center hover:scale-102 hover:bg-[#a7d87c] transition ${disabled ? "opacity-50  bg-[#A7D87C] cursor-not-allowed pointer-events-none" : "cursor-pointer"}`}>
+        {title}
+      </div>
+    )
+
+  }
 }
